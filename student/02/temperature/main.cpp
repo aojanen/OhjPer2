935de63394837588f5b6a5ab7c9fef09ef25d76c
Akseli Ojanen <akseli.ojanen@tuni.fi>
2024-01-11 22:06:29 +0200

    add temperature with second line print

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -9,5 +9,6 @@ int main()
     double temperature = 0;
     cin >> temperature;
     cout << temperature << " degrees Celsius is " << temperature * 1.8 + 32 << " degrees Fahrenheit" << endl;
+    cout << temperature << " degrees Fahrenheit is " << (temperature - 32) / 1.8 << " degrees Celsius" << endl;
     return 0;
 }
