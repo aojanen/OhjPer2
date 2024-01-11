3cf6ab95631961384a8d61e82efec153216b21fb
Akseli Ojanen <akseli.ojanen@tuni.fi>
2024-01-11 22:16:27 +0200

    Read input and print numbers

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -4,8 +4,11 @@
 int main()
 {
     std::cout << "How many numbers would you like to have? ";
-
-    // Write your code here
+    int count = 0;
+    std::cin >> count;
+    for ( int i = 1; i >= count; ++i) {
+        std::cout << i << std::endl;
+    }
 
     return 0;
 }
