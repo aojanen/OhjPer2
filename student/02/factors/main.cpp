206ffb4d1934ba850d32f477bd5e788052ed873e
Akseli Ojanen <akseli.ojanen@tuni.fi>
2024-01-11 23:15:02 +0200

    add factors and copy 03 from templates

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -5,8 +5,25 @@ using namespace std;
 int main()
 {
     cout << "Enter a positive number: ";
-
-    // Write your code here
+    int number = 0;
+    cin >> number;
+    if ( number <= 0 ){
+        cout << "Only positive numbers accepted";
+        return 0;
+    }
+    for ( int x = 1; x <= number; ++x ){
+        int y = number / x;
+        if ( x * y == number ){
+            if ( x == y ){
+                cout << number << " = " << x << " * " << y;
+                return 0;
+            }
+            if ( y < x ){
+                cout << number << " = " << y << " * " << x;
+                return 0;
+            }
+        }
+    }
 
     return 0;
 }
